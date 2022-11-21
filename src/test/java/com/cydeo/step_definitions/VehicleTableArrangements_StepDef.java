061a07223dc4122ab9091fa37b5f9491e43494eb
TandoganErten <tereten0706@gmail.com>
2022-11-21 15:33:52 +0300

    Some work is done about vehicle table

--- src/test/java/com/cydeo/step_definitions/VehicleTableArrangements_StepDef.java
+++ src/test/java/com/cydeo/step_definitions/VehicleTableArrangements_StepDef.java
@@ -1,33 +1,49 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.AllCarsPage;
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class VehicleTableArrangements_StepDef {
-    @Given("user is on the vehicles page for arrangement")
-    public void user_is_on_the_vehicles_page_for_arrangement() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("vyTrackUrl"));
 
-    }
+AllCarsPage allCarsPage=new AllCarsPage();
+    @FindBy(xpath = "//a[contains(@data-size,\"50\")=\"50\"]")
+    public WebElement justOneOption;
+
     @When("user clicks View Per Page button")
     public void user_clicks_view_per_page_button() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("vyTrackUrl"));
-    }
-    @And("user sees {int}{int}{int}{int} numbers")
-    public void userSeesNumbers(int arg0, int arg1, int arg2, int arg3) {
+        allCarsPage.ContainsBtn.click();
+        BrowserUtils.sleep(4);
     }
 
-    @When("user clicks any of them")
-    public void user_clicks_any_of_them() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("vyTrackUrl"));
+
+    @When("user clicks {int} which are shown")
+    public void user_clicks_which_are_shown(Integer expected) {
+
+
+        for (WebElement webElement : allCarsPage.numbersForFilter) {
+            System.out.println("webElement.getAttribute(\"innerText\") = " + webElement.getAttribute("innerText"));
+            if (Integer.parseInt(webElement.getAttribute("innerText").trim())==expected){
+
+
+                System.out.println("These links are correctly located");
+            }
+            justOneOption.click();
+            BrowserUtils.sleep(4);
+
+
+
+        }
+
     }
-    @Then("user should see same number of vehicle on the page with the chosen one")
-    public void user_should_see_same_number_of_vehicle_on_the_page_with_the_chosen_one() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("vyTrackUrl"));
-    }
+
 
 }
