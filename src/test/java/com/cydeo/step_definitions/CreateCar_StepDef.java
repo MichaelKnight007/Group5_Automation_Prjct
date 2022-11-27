960cd6d58c41b5c2d42cc8dfee019d6f0b9f9b34
Ersin KAYHAN <dekked2438@gmail.com>
2022-11-27 23:20:45 +0300

    AC-2 is finished

--- src/test/java/com/cydeo/step_definitions/CreateCar_StepDef.java
+++ src/test/java/com/cydeo/step_definitions/CreateCar_StepDef.java
@@ -2,30 +2,25 @@ package com.cydeo.step_definitions;
 
 import com.cydeo.pages.AllCarsPage;
 import com.cydeo.pages.CreateCarBasePage;
-import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.But;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CreateCar_StepDef {
 
-    AllCarsPage allCarsPage=new AllCarsPage();
+    AllCarsPage allCarsPage = new AllCarsPage();
+    CreateCarBasePage createCarBasePage = new CreateCarBasePage();
 
     @Then("Verify driver cannot see the button")
     public void driver_Cannot_See_Button() {
 
-       int sizeOfBtn = allCarsPage.createCarBtn.size();
+        int sizeOfBtn = allCarsPage.createCarBtn.size();
 
-        Assert.assertEquals(1,sizeOfBtn);
+        Assert.assertEquals(1, sizeOfBtn);
 
 
         /*boolean createCarIsEmpty=Driver.getDriver().findElements(By.xpath("//a[@title='Create Car']")).isEmpty();
@@ -66,27 +61,72 @@ public class CreateCar_StepDef {
         /*finally {
             System.out.println("Forcefully Driver Cannot Create Car Test is Passed!");
         }*/
-                System.out.println("Geçti mi?");
+        System.out.println("Geçti mi?");
 
-            }
+    }
 
 
-            @But("that the sale manager and store manager can click Create Car button")
-            public void verifyThatTheSaleManagerAndStoreManagerSeeButton () {
+    @But("that the sale manager and store manager can click Create Car button")
+    public void verifyThatTheSaleManagerAndStoreManagerSeeButton() {
 
-                Assert.assertTrue(allCarsPage.createCarBtn.get(0).isEnabled());
+        Assert.assertTrue(allCarsPage.createCarBtn.get(0).isEnabled());
 
-            }
+    }
 
 
     @And("User goes to Create Car page and creates a car by filling compulsory fields")
     public void userGoesToCreateCarPageAndCreatesACarByFillingCompulsoryFields() {
 
+        CreateCarBasePage.createCar();
+        String entitySavedMessageText = createCarBasePage.entitySavedMessage.getText();
+        Assert.assertEquals("Entity saved",entitySavedMessageText);
 
+    }
+
+    @Then("User goes to Create Car page and enters Compulsory fields without complying with the conditions")
+    public void userGoesToCreateCarPageAndEntersCompulsoryFieldsWithoutComplyingWithTheConditions() {
+
+        // LicencePlateField Assertion
         List<String> textOfLicencePlateFieldList = CreateCarBasePage.licencePlateFieldCheck();
-
-
         Assert.assertEquals(textOfLicencePlateFieldList.get(0), textOfLicencePlateFieldList.get(1));
+
+        // DriverField Assertion
+        List<String> textOfDriverFieldList = CreateCarBasePage.driverFieldCheck();
+        Assert.assertEquals(textOfDriverFieldList.get(0), textOfDriverFieldList.get(1));
+
+        // Chassis Number Field Assertion
+        List<String> textOfChassisNumberFieldList=CreateCarBasePage.chassisNumberFieldCheck();
+        Assert.assertEquals(textOfChassisNumberFieldList.get(0),textOfChassisNumberFieldList.get(1));
+
+        // Model Year Field Assertion
+        List<String> textOfModelYearFieldList=CreateCarBasePage.modelYearFieldCheck();
+        Assert.assertEquals(textOfModelYearFieldList.get(0),textOfModelYearFieldList.get(1));
+
+        // Last Odometer Field Assertion
+        List<String> textOflastOdometerFieldList = CreateCarBasePage.lastOdometerFieldCheck();
+        Assert.assertEquals(textOflastOdometerFieldList.get(0),textOflastOdometerFieldList.get(1));
+
+        // Tags_Employee Assertion
+        CreateCarBasePage.tags_EmployeeTest();
+        boolean tags_EmployeeClicked = createCarBasePage.tags_Employee.isSelected();
+        Assert.assertTrue(tags_EmployeeClicked);
+
+        // Tags_JuniorField Assertion
+        CreateCarBasePage.tags_JuniorField();
+        boolean tags_JuniorFieldClicked = createCarBasePage.tags_JuniorField.isSelected();
+        Assert.assertTrue(tags_JuniorFieldClicked);
+
+        // Manuel Transmission Btn Assertion
+        CreateCarBasePage.manuelTransmissionBtnTest();
+        boolean manuelTransmissonBtnClicked = createCarBasePage.manuelTransmissonBtn.isSelected();
+        Assert.assertTrue(manuelTransmissonBtnClicked);
+
+        // Diesel Btn Assertion
+        CreateCarBasePage.dieselBtnTest();
+        boolean dieselBtnlicked = createCarBasePage.dieselBtn.isSelected();
+        Assert.assertTrue(dieselBtnlicked);
+
+
 
 
     }
