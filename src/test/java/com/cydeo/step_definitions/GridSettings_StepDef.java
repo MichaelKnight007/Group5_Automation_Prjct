bd2a350c721f4b2548650d9995c1983b2b410c93
ozergungor <ozergungor2019@gmail.com>
2022-11-28 07:35:47 -0500

    gungor Grid Settings

--- src/test/java/com/cydeo/step_definitions/GridSettings_StepDef.java
+++ src/test/java/com/cydeo/step_definitions/GridSettings_StepDef.java
@@ -16,6 +16,7 @@ public class GridSettings_StepDef {
     GridSettingsPage gridSettingsPage = new GridSettingsPage();
     @And("The user clicks on Grid Settings button from the top right menu")
     public void theUserClicksOnButtonFromTheTopRightMenu() {
+        allCarsPage.waitUntilLoaderScreenDisappear();
         allCarsPage.clickGridSettings();
     }
 
@@ -31,12 +32,10 @@ public class GridSettings_StepDef {
         System.out.println("expected options..: " + expectedOptions);
         Assert.assertEquals(expectedOptions, actualOptions);
     }
-
     @When("The user types following column names into quick search field")
     public void the_user_types_following_column_names_into_quick_search_field(List<String> columns) {
         gridSettingsPage.findCollumn(columns);
     }
-
     @Then("The relevant column shows up in the grid settings")
     public void the_relevant_column_shows_up_in_the_grid_settings() {
         //there is nothing inside this method,
@@ -44,14 +43,11 @@ public class GridSettings_StepDef {
         //This situation is kind of limitation of Cucumber
         //Sometimes scenarios can not be divided
     }
-
-
     @When("The user clicks on the following columns from grid settings")
     public void the_user_clicks_on_the_following_columns_from_grid_settings(List<String> columns) {
         gridSettingsPage.deselectAllColumns();
         gridSettingsPage.clickColumn(columns);
     }
-
     @Then("The relevant column shows up in the fleet - vehicles table")
     public void the_relevant_column_shows_up_in_the_fleet_vehicles_table() {
         //there is nothing inside this method,
@@ -59,18 +55,12 @@ public class GridSettings_StepDef {
         //This situation is kind of limitation of Cucumber
         //Sometimes scenarios can not be divided
     }
-
-
     @When("The user can drag {string} column and drop to the index {int}")
     public void the_user_can_drag_column_and_drop_to_the_index(String column, Integer index) {
         gridSettingsPage.dragAndDrop(column,index);
     }
-
     @Then("{string} column is positioned at index {int}")
     public void column_is_positioned_at_index(String column, Integer index) {
         gridSettingsPage.verifyColumnPosition(column,index);
     }
-
-
-
 }
