747bf014c27a5899fbf78205ae4a31eeb8194010
Ersin KAYHAN <dekked2438@gmail.com>
2022-12-01 18:21:13 +0300

    Compulsory Fields AC is Solved

--- src/test/java/com/cydeo/step_definitions/CreateCar_StepDef.java
+++ src/test/java/com/cydeo/step_definitions/CreateCar_StepDef.java
@@ -2,11 +2,15 @@ package com.cydeo.step_definitions;
 
 import com.cydeo.pages.AllCarsPage;
 import com.cydeo.pages.CreateCarBasePage;
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.But;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
+import org.openqa.selenium.interactions.Actions;
 
+import javax.swing.*;
 import java.util.List;
 
 public class CreateCar_StepDef {
@@ -67,19 +71,19 @@ public class CreateCar_StepDef {
 
         // DriverField Assertion
         List<String> textOfDriverFieldList = CreateCarBasePage.driverFieldCheck();
-        Assert.assertEquals(textOfDriverFieldList.get(0), textOfDriverFieldList.get(1));
+        Assert.assertEquals(textOfDriverFieldList.get(2), textOfDriverFieldList.get(3));
 
         // Chassis Number Field Assertion
         List<String> textOfChassisNumberFieldList=CreateCarBasePage.chassisNumberFieldCheck();
-        Assert.assertEquals(textOfChassisNumberFieldList.get(0),textOfChassisNumberFieldList.get(1));
+        Assert.assertEquals(textOfChassisNumberFieldList.get(4),textOfChassisNumberFieldList.get(5));
 
         // Model Year Field Assertion
         List<String> textOfModelYearFieldList=CreateCarBasePage.modelYearFieldCheck();
-        Assert.assertEquals(textOfModelYearFieldList.get(0),textOfModelYearFieldList.get(1));
+        Assert.assertEquals(textOfModelYearFieldList.get(6),textOfModelYearFieldList.get(7));
 
         // Last Odometer Field Assertion
         List<String> textOflastOdometerFieldList = CreateCarBasePage.lastOdometerFieldCheck();
-        Assert.assertEquals(textOflastOdometerFieldList.get(0),textOflastOdometerFieldList.get(1));
+        Assert.assertEquals(textOflastOdometerFieldList.get(8),textOflastOdometerFieldList.get(9));
 
         // Tags_Employee Assertion
         CreateCarBasePage.tags_EmployeeTest();
@@ -107,10 +111,22 @@ public class CreateCar_StepDef {
     @Then("User can select ‘Vehicle Model’ and ‘Vehicle Make’ from the list")
     public void userCanSelectVehicleModelAndVehicleMakeFromTheList() {
 
-        List<Boolean> vehicleModelMakeClikable=CreateCarBasePage.vehicleModel_MakeClickableTest();
+        List<Boolean> areVehicle_Model_Make_DoubleSelected=createCarBasePage.vehicleModel_MakeClickableTest();
 
-       Assert.assertTrue(vehicleModelMakeClikable.get(0));
-       Assert.assertTrue(vehicleModelMakeClikable.get(1));
+       Assert.assertFalse(areVehicle_Model_Make_DoubleSelected.get(0));
+       Assert.assertFalse(areVehicle_Model_Make_DoubleSelected.get(1));
+
+
+    }
+
+    @Then("User can see newly created car in ’Fleet-Vehicle’ table")
+    public void userCanSeeNewlyCreatedCarInFleetVehicleTable() {
+
+        createCarBasePage.see_Newly_Created_Car_In_Fleet_Vehicle_Table();
+        createCarBasePage.licencePlateHeader.click();
+        BrowserUtils.sleep(2);
+        String createdCarText=createCarBasePage.carCreated.getText();
+        Assert.assertEquals("01 DR 805",createdCarText);
 
 
     }
